b96fe2656aea0494a1649d1f5972f0a82ef1fdbf
mbronek7 <mbronek7@gmail.com>
2018-12-03 06:02:03 +0100

    Migrate to Webpacker

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -2,9 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
+
 
 import Rails from "rails-ujs"
 Rails.start()
@@ -17,3 +15,8 @@ ActiveStorage.start()
 
 import "channels"
 import "actiontext"
+import "bootstrap/dist/js/bootstrap";
+
+$(function () {
+  console.log('Hello World from Webpacker');
+});
