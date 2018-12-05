63b35c2e90d8b994967ba269a40f9f9c6c04450c
mbronek7 <mbronek7@gmail.com>
2018-12-05 01:58:45 +0100

    Fix ppstgres problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
+    if @user.update_attributes(user_params)
       redirect_to root_path, notice: "Your profile was successfully updated."
     else
       render action: :edit
