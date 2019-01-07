2165623fdfb03e6fc56dd754ac2c011395a1fe49
mbronek7 <mbronek7@gmail.com>
2019-01-07 20:07:01 +0100

    Fix User Validator

--- app/validators/user_validator.rb
+++ app/validators/user_validator.rb
@@ -11,8 +11,7 @@ class UserValidator
     required(:user).schema do
       required(:name).filled(max_size?: 100)
       required(:email).filled(format?: EMAIL_REGEX)
-      required(:password).filled(eql?: :password_confirmation)
-      required(:password).filled(min_size?: 6)
+      required(:password).filled(min_size?: 6, eql?: :password_confirmation)
       required(:password_confirmation).value(:filled?)
     end
   end
