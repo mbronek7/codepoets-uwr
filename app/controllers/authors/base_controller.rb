8491a866ea8e4997e5d5b9da16fc290b760dcb11
mbronek7 <mbronek7@gmail.com>
2019-01-12 19:53:49 +0100

    Add transactions

--- app/controllers/authors/base_controller.rb
+++ app/controllers/authors/base_controller.rb
@@ -3,13 +3,5 @@
 module Authors
   class BaseController < ApplicationController
     before_action :authorize
-
-    def create_flash(errors)
-      flash = []
-      errors.each do |error|
-        flash << error[1].first
-      end
-      flash.join("<br />".html_safe.to_s)
-    end
   end
 end
